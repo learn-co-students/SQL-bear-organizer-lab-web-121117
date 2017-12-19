a934fdda556012ac347add0c704b30095ad3ca15
github username <github email address>
2017-12-19 09:15:12 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+-- * Mr. Chocolate
+-- * Rowdy
+-- * Tabitha
+-- * Sergeant Brown
+-- * Melissa
+-- * Grinch
+-- * Wendy
+-- * unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
+
+INSERT INTO bears (name, age, gender,color,temperament,alive) VALUES ('Mr. Chocolate', 4, 'M','Brown', 'chill',1);
+INSERT INTO bears (name, age, gender,color,temperament,alive) VALUES ('Rowdy', 4, 'M','Brown','chill',0);
+INSERT INTO bears (name, age, gender,color,temperament,alive) VALUES ('Tabitha', 4, 'F','Black','chill',1);
+INSERT INTO bears (name, age, gender,color,temperament,alive) VALUES ('Sergeant Brown', 7, 'M','Grey','chill',1);
+INSERT INTO bears (name, age, gender,color,temperament,alive) VALUES ('Melissa', 9, 'F','Brown','chill',1);
+INSERT INTO bears (name, age, gender,color,temperament,alive) VALUES ('Grinch', 3, 'F','Black','chill',1);
+INSERT INTO bears (name, age, gender,color,temperament,alive) VALUES ('Wendy', 3, 'F','Black','chill',1);
+INSERT INTO bears (name, age, gender,color,temperament,alive) VALUES (null, 6, 'M','Black','not-chill',0);
