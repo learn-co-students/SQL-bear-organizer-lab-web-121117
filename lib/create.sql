62c6e875da017f6de7fa38b043dd75b4293f4863
github username <github email address>
2017-12-20 10:14:38 -0500

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+     name TEXT,
+     age INTEGER,
+     gender CHAR(1),
+     color TEXT,
+     temperament TEXT,
+     alive BOOLEAN
+);
+
+-- (8, null, 20, "M", "black", "aggressive", 0);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, breed, gender, color,temperament, alive) VALUES ('Mr. Chocolate', 1, 'Black Bear', 'M', 'black', 'cuddly', 1);
+INSERT INTO bears (name, age, breed, gender, color,temperament, alive) VALUES ('Rowdy', 2, 'Black Bear', 'M', 'black', 'cuddly', 0);
+INSERT INTO bears (name, age, breed, gender, color,temperament, alive) VALUES ('Tabitha', 3, 'Black Bear', 'M', 'black', 'cuddly', 1);
+INSERT INTO bears (name, age, breed, gender, color,temperament, alive) VALUES ('Sergeant Brown', 4, 'Black Bear', 'F', 'black', 'cuddly', 0);
+INSERT INTO bears (name, age, breed, gender, color,temperament, alive) VALUES ('Melissa', 5, 'Black Bear', 'M', 'black', 'cuddly', 1);
+INSERT INTO bears (name, age, breed, gender, color,temperament, alive) VALUES ('Grinch', 6, 'Black Bear', 'F', 'black', 'cuddly', 0);
+INSERT INTO bears (name, age, breed, gender, color,temperament, alive) VALUES ('Wendy', 7, 'Black Bear', 'M', 'black', 'cuddly', 1);
+INSERT INTO bears (name, age, breed, gender, color,temperament, alive) VALUES (null, 8, 'Black Bear', 'M', 'black', 'cuddly', 0);
